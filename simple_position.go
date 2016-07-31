11901594e57a7bb81164de760b0a0f1749ddb253
Samuel Sweeney <sams@uber.com>
2016-07-31 12:58:55 -0700

    Fix SimplePosition quantity

--- simple_position.go
+++ simple_position.go
@@ -6,7 +6,7 @@ import (
 
 type SimplePosition struct {
 	PurchaseTime    time.Time `json:"purchase_time"`
-	Quantity        int       `json:"quantity"`
+	Quantity        float64   `json:"quantity"`
 	Symbol          string    `json:"symbol"`
 	AverageBuyPrice float64   `json:"average_buy_price"`
 	LastTradePrice  float64   `json:"last_trade_price"`
